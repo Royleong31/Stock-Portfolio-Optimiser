e760cf286557eb57cd17f05f7fc3da7d6d085b4d
anhngoc <ngoc2162003@gmail.com>
2022-04-23 15:45:57 +0800

    Add comments

--- utils/getStockData.py
+++ utils/getStockData.py
@@ -1,38 +1,39 @@
 import warnings
 import itertools
 import numpy as np
-import matplotlib.pyplot as plt
-# warnings.filterwarnings("ignore")
-# plt.style.use('fivethirtyeight')
 import pandas as pd
 import statsmodels.api as sm
-import matplotlib
+import matplotlib.pyplot as plt
 from pandas_datareader import data as web
 
+
 def getStockData(name, startDate, endDate):
-    stock2=(web.DataReader(name, data_source='yahoo', start = startDate, end = endDate))['Close']
-    stock2=pd.DataFrame(stock2)
-    stock2 = stock2.sort_values('Date')
-    type(stock2)
+    # Only take the columns we need ("Date" and  "Close")
+    stock = (web.DataReader(name, data_source="yahoo", start=startDate, end=endDate))[
+        "Close"
+    ]
+    stock = pd.DataFrame(stock)
+    stock = stock.sort_values("Date")
+    type(stock)
 
-    stock2.isnull().sum()
-    stock2 = stock2.groupby('Date')['Close'].sum().reset_index()
-    stock2
-    stock2.head()
+    # Check for missing value
+    stock.isnull().sum()
 
-    stock2.Date = pd.to_datetime(stock2.Date, format='%Y%m%d', errors='ignore')
-    stock2 = stock2.set_index('Date')
-    stock2.index = pd.to_datetime(stock2.index)
+    # Indexing the data by date for future use
+    stock = stock.groupby("Date")["Close"].sum().reset_index()
+    stock.Date = pd.to_datetime(stock.Date, format="%Y%m%d", errors="ignore")
+    stock = stock.set_index("Date")
+    stock.index = pd.to_datetime(stock.index)
 
-    return pd.DataFrame(stock2.Close.resample('M').mean())
+    # Resample the stock by Monthly mean value for smoother plot and better prediction
+    return pd.DataFrame(stock.Close.resample("M").mean())
+
 
 def getMultipleStocks(stocks, startDate, endDate):
     prices = pd.DataFrame()
-    
+
     for stock in stocks:
         temp = getStockData(stock, startDate, endDate)
         prices = pd.concat([prices, temp], axis=1).reindex(temp.index)
     prices.columns = stocks
     return prices
-    
-    
